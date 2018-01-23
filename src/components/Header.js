38bdd2feac6bdbb1a146b89cd52580cdd3d16310
Alex Steele <alexsteele110@gmail.com>
2018-01-23 11:58:25 -0500

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-diamond" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Alex Steele</h1>
+        <p>Portfolio for a passionate full stack web developer.</p>
+        <ul className="icons">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/asteele110/"
+              className="icon fa-linkedin-square"
+            >
+              <span className="label">Linkedin</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:alexsteele110@gmail.com"
+              className="icon fa-envelope-o"
+            >
+              <span className="label">Email</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/alexsteele110"
+              className="icon fa-github"
+            >
+              <span className="label">GitHub</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Skills
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: React.PropTypes.func,
+  timeout: React.PropTypes.bool,
+}
+
+export default Header
